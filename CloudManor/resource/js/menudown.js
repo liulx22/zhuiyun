0dc3ee0b63ba5de92056b1a8fcfbdc8470aac7e0
zhihui <1601457588@qq.com>
2020-08-19 12:25:41 +0800

    孙智慧-历史订单

--- CloudManor/resource/js/menudown.js
+++ CloudManor/resource/js/menudown.js
@@ -14,14 +14,51 @@ $(function(){
     		$(".select_first_ul>li>p").css("width","100%");
     	}
     })
+	var shijian=null;//时间-筛选值
+	var leixing=null;//类型-筛选值
 	$(".select_first_ul>li>p").click(function(){
 		$(".select_second_ul").hide();
 		$(this).parent("li").addClass("focus").siblings("li").removeClass("focus");
 		var ynul = $(this).parent("li").has(".select_second_ul").length;
         if(ynul == 0){
-        	
-        	var choose = $(this).text();
-			$(this).parents(".select_textul").siblings(".select_textdiv").find(".s_text").text(choose);
+        	var choose = $(this).text();//点击的值
+			var bia = $(this).parents(".select_textul").siblings(".select_textdiv").find(".s_text").text();//筛选类型
+			if(choose!="全部"){//判断是否点击了全部
+				if(bia.indexOf("(")==-1){//判断一下筛选类型后面是否有括号
+					if(bia=="时间"){//判断一下点击的筛选类型
+						shijian=choose;//赋值
+					}else if(bia=="类型"){
+						leixing=choose;
+					}
+					$(this).parents(".select_textul").siblings(".select_textdiv").find(".s_text").text(bia+"( "+choose+" )");
+				}else{
+					var biao=bia.substring(0,bia.indexOf("("));
+					if(biao=="时间"){//判断一下点击的筛选类型
+						shijian=choose;//赋值
+					}else if(biao=="类型"){
+						leixing=choose;
+					}
+					$(this).parents(".select_textul").siblings(".select_textdiv").find(".s_text").text(biao+"( "+choose+" )");
+				}
+			}else{
+				if(bia.indexOf("(")==-1){
+					if(bia=="时间"){//判断一下点击的筛选类型
+						shijian=null;//赋值
+					}else if(bia=="类型"){
+						leixing=null;
+					}
+					$(this).parents(".select_textul").siblings(".select_textdiv").find(".s_text").text(bia);
+				}else{
+					var biao=bia.substring(0,bia.indexOf("("));
+					if(biao=="时间"){//判断一下点击的筛选类型
+						shijian=null;//赋值
+					}else if(biao=="类型"){
+						leixing=null;
+					}
+					$(this).parents(".select_textul").siblings(".select_textdiv").find(".s_text").text(biao);
+				}
+			}
+			orderdingdan(shijian,leixing);//带筛选条件进行查询
 			$(this).parents(".select_textul").siblings("input").val(choose);
 			$(this).parents(".select_textul").fadeOut(300);
         }else{
